8303b9610c3d6df315d19056dce95715cd534133
Saransh khobragade <skhobragade@atlassian.com>
2021-05-25 19:09:16 +0530

    useReducer done

--- react_hook_basic/src/App.js
+++ react_hook_basic/src/App.js
@@ -1,37 +1,27 @@
-import { useCallback, useState } from 'react'
-import Title from './Title';
-import Increment from './Increment'
-import Increment2 from './Increment2'
+import { useReducer } from 'react'
+
+function reducer(state,actions){
+  switch(actions.type){
+    case 'inc': return state + 1;
+    case 'dec': return state - 1;
+    default : return state;
+  }
+}
+
+const intialState = 0
 
 const  App = () => {
 
-  const [count1,setCount1] = useState(0)
-  const [count2,setCount2] = useState(0)
-  
-  const inc1 = useCallback((count)=>{
-    setCount1(prev => prev + count)         // This version will only change when setCount1/count1 changes
-  },[setCount1])
+  const [count,dispatch] = useReducer(reducer,intialState)
+  //dispatch will take actions and state will mutate by reducer functions
 
-  const inc2 = useCallback((count)=>{
-    setCount2(prev => prev + count)         // This version will only change when setCount2/count12 changes
-  },[setCount2])
-  
-    return (
+  return (
     <div>
-      <Title/>  {/* Will not change oncs render because no props changes*/}
-      
-      <Increment fire={inc1}/>   {/* Will not change if inc2 update its state count2 but setCount1 version not changed so no prop changed */}
-
-      <Increment2 fire={inc2}/> {/* Will not change if inc1 update its state count1 but setCount2 version not changed so no prop changed */}
-
-      Count1->{count1}
-      <br/>
-      Count2->{count2}
+      Count:{count}
+      <button onClick={()=>dispatch({type:'inc'})}>INC</button>
+      <button onClick={()=>dispatch({type:'dec'})}>DEC</button>
     </div>
   );
 }
 
 export default App;
-
-//By using useCallback and react.memo() we can save re-render alot,because every state changes call for parent component to re-render and new versions
-//of functions created so child also re-renders
